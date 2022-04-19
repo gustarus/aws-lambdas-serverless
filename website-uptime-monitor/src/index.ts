ddd98303f884613ba396d3688ab504982f42e818
gustarus <gustarus@gmail.com>
2022-04-19 11:06:34 +0200

    improve uptime monitor services

--- website-uptime-monitor/src/index.ts
+++ website-uptime-monitor/src/index.ts
@@ -1,12 +1,29 @@
-// use url library to parse the url from the parameters
 import * as url from 'url';
+import * as uuid from 'uuid';
+import * as Bunyan from 'bunyan';
+import { Context } from 'aws-lambda';
 
 const {
+  AWS_LAMBDA_FUNCTION_NAME: functionName,
+  LOG_LEVEL: logLevel = Bunyan.TRACE,
   TARGET_URL: targetUrl,
 } = process.env;
 
+let logger: Bunyan;
+
 // implement aws lambda handler
-exports.handler = function(event, context, callback) {
+export async function handler(event: any, context: Context) {
+  // create logger if not created
+  logger = logger || Bunyan.createLogger({
+    name: `lambda.${functionName}`,
+    level: <Bunyan.LogLevel>logLevel,
+  });
+
+  // create logger for the specified request
+  const { awsRequestId: requestId = uuid.v4() } = context;
+  const log = logger.child({ requestId }, true);
+  log.info({ event, context }, 'run');
+
   // get target url details
   const targetParsed = url.parse(targetUrl.trim());
   const protocol = targetParsed.protocol.substring(0, targetParsed.protocol.length - 1);
@@ -15,24 +32,19 @@ exports.handler = function(event, context, callback) {
   const protocolModule = require(protocol);
 
   // send request from desired protocol module
-  console.log('Check status of the "' + targetUrl.trim() + '"');
-  const request = protocolModule.request(targetParsed, function(buffer) {
-    buffer.setEncoding('utf8');
+  log.info(`Send request to url "${targetUrl.trim()}"`);
+  await new Promise((resolve, reject) => {
+    const request = protocolModule.request(targetParsed, (buffer) => {
+      const status = buffer.statusCode
+        ? parseInt(buffer.statusCode, 10) : undefined;
+      if (status !== 200) {
+        return reject(new Error(`Response returned invalid status code "${status}"`));
+      }
 
-    buffer.on('data', function(chunk) {
-      console.log('Read body chunk');
+      log.info('Server returned a valid status code');
     });
 
-    buffer.on('end', function() {
-      console.log('Response end');
-      callback();
-    });
+    request.on('error', reject);
+    request.end();
   });
-
-  request.on('error', function(e) {
-    console.error(e);
-    callback(e);
-  });
-
-  request.end();
-};
+}
